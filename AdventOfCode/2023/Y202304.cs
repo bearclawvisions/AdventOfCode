3aff10fc844f7470e3fcd68947b96de7a21a5380
tschoolderman <t.d.schoolderman@gmail.com>
2024-05-04 18:39:09 +0200

    Solved Part one day 4

---
+++ AdventOfCode/2023/Y202304.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode._2023;
+
+public class Y202304(IHelper helper)
+{
+    public void Run()
+    {
+        // Part One
+        Console.WriteLine("Part One: " + Scratchcards());
+
+        // Part Two
+        Console.WriteLine("Part Two: ");
+    }
+    
+    private int Scratchcards()
+    {
+        var inputPath = helper.GetInputFilePath(2023, 4);
+        using var reader = new StreamReader(inputPath);
+        var line = reader.ReadLine();
+
+        var totalPoints = 0;
+        while (!string.IsNullOrWhiteSpace(line))
+        {
+            var splitCardNumber = line.Split(":");
+            // var cardNumber = int.Parse(splitCardNumber[0].Split(" ")[1]);
+            var winningNumbers = splitCardNumber[1].Split("|")[0]
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+            
+            var lotteryNumbers = splitCardNumber[1].Split("|")[1]
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            totalPoints += CheckWinningNumbers(winningNumbers, lotteryNumbers);
+            
+            line = reader.ReadLine();
+        }
+        reader.Close();
+
+        return totalPoints;
+    }
+
+    private static int CheckWinningNumbers(List<int> winningNumbers, List<int> lotteryNumbers)
+    {
+        var matchingNumbers = lotteryNumbers.Intersect(winningNumbers).Count();
+        switch (matchingNumbers)
+        {
+            case 0:
+                return 0;  // no points
+            case 1:
+                return 1; // 1 match, 1 point
+        }
+
+        var cardPoints = 1;
+        while (matchingNumbers > 1)  // first match not doubled
+        {
+            cardPoints *= 2;
+            matchingNumbers--;
+        }
+
+        return cardPoints;
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,5 +4,5 @@ using AdventOfCode._2023;
 IHelper helper = new Helper();
 
 // 2023
-var y2023 = new Y202303(helper);
+var y2023 = new Y202304(helper);
 y2023.Run();
