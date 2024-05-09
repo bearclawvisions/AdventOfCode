1c024a1ec16aae513d6a019e26d1c3e865e4e7ea
tschoolderman <t.d.schoolderman@gmail.com>
2024-05-09 16:29:53 +0200

    small refactor of baseclass

--- AdventOfCode/AoCBase.cs
+++ AdventOfCode/AoCBase.cs
@@ -4,20 +4,11 @@ public class AoCBase
 {
     public void Run()
     {
-        // Part One
         Console.WriteLine("Part One: " + PartOne());
-
-        // Part Two
         Console.WriteLine("Part Two: " + PartTwo());
     }
-    
-    protected virtual int PartOne()
-    {
-        return 0;
-    }
 
-    protected virtual int PartTwo()
-    {
-        return 0;
-    }
+    protected virtual int PartOne() { return 0; }
+
+    protected virtual int PartTwo() { return 0; }
 }
