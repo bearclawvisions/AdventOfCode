67703ed657e3419b17ff82d09dc5e1fbf41191fb
BearclawVisions <bearclawvisions.git@proton.me>
2026-09-04 18:12:42 +0200

    refactor 2025 day 8 part 1 and solve part 2

--- AdventOfCode/2025/Y202508.cs
+++ AdventOfCode/2025/Y202508.cs
@@ -4,22 +4,67 @@ namespace AdventOfCode._2025;
 
 public class Y202508 : AoCBase
 {
-    private int ConnectionLimit = 1000;
-    // private int ConnectionLimit = 10; // for test data
-    
     public override string PartOne(string input)
     {
         // this based on the kruskal algorithm
         var lines = input.ToArrayInput();
 
+        var coords = GetCoords(lines);
+        
+        // get all box pairs to compare closest for a circuit
+        var pairs = GetPairsSortedByDistanceAsc(coords);
+        
+        var uf = new UnionFind(coords.Count);
+        ConnectPairs(uf, pairs);
+
+        var circuitSizes = ComputeCircuitSizes(uf, coords.Count);
+        
+        var answer = circuitSizes.Values
+            .OrderByDescending(v => v)
+            .Take(3)
+            .Aggregate(1L, (acc, v) => acc * v);
+
+        return answer.ToString();
+    }
+    
+    public override string PartTwo(string input)
+    {
+        var lines = input.ToArrayInput();
+        var coords = GetCoords(lines);
+        var pairs = GetPairsSortedByDistanceAsc(coords);
+        var uf = new UnionFind(coords.Count);
+        
+        var junctionBoxCount = coords.Count;
+        long answer = 0;
+        foreach (var pair in pairs)
+        {
+            if (junctionBoxCount == 1) break;
+    
+            // Union returns true when two *different* circuits merge
+            if (!uf.Union(pair.indexFirst, pair.indexSecond)) continue;
+            
+            junctionBoxCount--;
+            var boxA = coords[pair.indexFirst];
+            var boxB = coords[pair.indexSecond];
+            answer = (long)boxA.X * (long)boxB.X;
+        }
+        
+        return answer.ToString();
+    }
+
+    private List<Vector3> GetCoords(string[] lines)
+    {
         var coords = new List<Vector3>();
         foreach (var line in lines)
         {
             var parts = line.Split(',');
             coords.Add(new Vector3(float.Parse(parts[0]), float.Parse(parts[1]), float.Parse(parts[2])));
         }
-        
-        // get all box pairs to compare closest for a circuit
+        return coords;
+    }
+
+    private List<(long distance, int indexFirst, int indexSecond)> GetPairsSortedByDistanceAsc(List<Vector3> coords)
+    {
         var pairs = new List<(long distance, int indexFirst, int indexSecond)>();
         for (int current = 0; current < coords.Count; current++)
         {
@@ -38,27 +83,20 @@ public class Y202508 : AoCBase
             return c != 0 ? c : a.indexSecond.CompareTo(b.indexSecond);
         });
         
-        var uf = new UnionFind(coords.Count);
-        ConnectPairs(uf, pairs);
-
-        var circuitSizes = ComputeCircuitSizes(uf, coords.Count);
-        
-        var answer = circuitSizes.Values
-            .OrderByDescending(v => v)
-            .Take(3)
-            .Aggregate(1L, (acc, v) => acc * v);
-
-        return answer.ToString();
+        return pairs;
     }
-    
+
     private void ConnectPairs(UnionFind uf, List<(long distance, int indexFirst, int indexSecond)> pairs)
     {
+        // testdata results in 190 pairs. quickfix like this for easy switch between test and real data
+        var connectionLimit = pairs.Count == 190 ? 10 : 1000; // 10 for test data, 1000 for real data
+        
         // pairs is sorted shortest-first, so we take them in order
         var processed = 0;
         foreach (var pair in pairs)
         {
             // distance is irrelevant since we sorted
-            if (processed >= ConnectionLimit) break;
+            if (processed >= connectionLimit) break;
             processed++;
 
             uf.Union(pair.indexFirst, pair.indexSecond);
@@ -106,18 +144,19 @@ public class Y202508 : AoCBase
         }
 
         // merge the circuits. Attach the smaller tree under the larger
-        public void Union(int a, int b)
+        public bool Union(int a, int b)
         {
             var rootA = Find(a); // root/circuit of box a
             var rootB = Find(b); // root/circuit of box b
 
-            if (rootA == rootB) return; // same circuit
+            if (rootA == rootB) return false; // same circuit
 
             if (_size[rootA] < _size[rootB])
                 (rootA, rootB) = (rootB, rootA);
 
             _parent[rootB] = rootA; // box b's whole circuit now points to rootA
             _size[rootA] += _size[rootB]; // box a's circuit grew by box b's member count
+            return true;
         }
     }
 }
